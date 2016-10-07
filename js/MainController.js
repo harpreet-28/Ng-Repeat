5d84b213b5627076dd7b2113b62f0f759bf720be
Harpreet Kaur <preet.k.k88@gmail.com>
2016-10-06 23:54:07 -0400

    Ng Repeat

---
+++ js/MainController.js
@@ -0,0 +1,26 @@
+var MovieController = function MovieController($scope) {
+
+    // var self = this;
+    $scope.favorites = [{
+        title: 'The Shawshank Redemption',
+        year: '1994'
+    }, {
+        title: 'Inception',
+        year: '2010'
+    }, {
+        title: 'The Matrix',
+        year: '1999'
+    }, {
+        title: 'Saving Private Ryan',
+        year: '1998'
+    }, {
+        title: 'The Aviator',
+        year: '2004'
+    }];
+    //console.log(self.favorites);
+};
+
+
+angular
+    .module('app')
+    .controller('MovieController', ['$scope', MovieController]);
